d9e6c35460d62e00fe694a5f070da375f29c903d
juninhork <junioor.braga@gmail.com>
2017-05-16 15:20:07 -0300

    Correçao de erro ao abrir o Fragimento

--- app/src/main/java/com/example/ferra/coretoadmin/aplication/basepresenter/activity/FinanceiroActivity.java
+++ app/src/main/java/com/example/ferra/coretoadmin/aplication/basepresenter/activity/FinanceiroActivity.java
@@ -1,15 +1,15 @@
 package com.example.ferra.coretoadmin.aplication.basepresenter.activity;
 
 import android.os.Bundle;
+import android.support.v4.app.*;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.ferra.coretoadmin.R;
+import com.example.ferra.coretoadmin.panel.fragment.IndicadorGastoFragment;
 
 public class FinanceiroActivity extends AppCompatActivity {
 
-//    private PieChart pieChart;
-//    private IndicadorGastoFragment mIndicadorGastoFragment;
-//    LinearLayout mLinear;
+    private IndicadorGastoFragment mIndicadorGastoFragment;
 
 
     @Override
@@ -17,37 +17,14 @@ public class FinanceiroActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_financeiro);
 
-        getSupportFragmentManager().findFragmentById(R.id.tv_title);
+        FragmentManager mFragmentManager;
+        FragmentTransaction mFragmentTransaction;
 
-
-//        android.support.v4.app.FragmentManager mFragmentManager;
-//        android.support.v4.app.FragmentTransaction mFragmentTransaction;
-//
-//        mIndicadorGastoFragment = new IndicadorGastoFragment();
-//        mFragmentManager = getSupportFragmentManager();
-//        mFragmentTransaction = mFragmentManager.beginTransaction();
-//        mFragmentTransaction.replace(R.id.container, mIndicadorGastoFragment);
-//        mFragmentTransaction.commit();
-
-
-
-
-       // mFragmentTransaction.add(R.id.container,mIndicadorGastoFragment,"");
-        //mFragmentTransaction.commit();
-
-///        getSupportFragmentManager().beginTransaction()
-//                .add(R.id.container, new MenuFragment())
-//                .commit();
-//            getSupportFragmentManager().findFragmentById(R.id.chart_applications_distribution);
-//
-//        this.mView = (View) findViewById(R.id.container);
-//        mView = new View(getFragmentManager(), IndicadorGastoFragment);
-
-
+        mIndicadorGastoFragment = new IndicadorGastoFragment();
+        mFragmentManager = getSupportFragmentManager();
+        mFragmentTransaction = mFragmentManager.beginTransaction();
+        mFragmentTransaction.replace(R.id.container, mIndicadorGastoFragment);
+        mFragmentTransaction.commit();
 
     }
-
-
-
-
 }
